746bb259aee76b819610c730afb05854c29a2604
nathangong <nathangong1234@gmail.com>
2019-10-08 18:17:54 -0700

    Added Tetris

---
+++ src/games/tetris/Game.java
@@ -0,0 +1,9 @@
+import java.util.javax.Jpanel;
+
+import javax.swing.JFrame;
+
+public class Game extends Jpanel{
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Tetris");
+    }
+}
