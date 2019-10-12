b2b1a22f86c130c669ed65afe91767d40a834c4f
Shaunak Warty <37036096+hyperXcube@users.noreply.github.com>
2019-10-11 18:52:23 -0700

    (Tetris) Update constants

--- src/games/tetris/Constants.java
+++ src/games/tetris/Constants.java
@@ -1,8 +1,8 @@
 package games.tetris;
 
 class Constants {
-    static final int TILE = 20;
-    static final int WIDTH = TILE * 30;
-    static final int HEIGHT = TILE * 40;
+    static final int TILE = 40;
+    static final int WIDTH = TILE * 12;
+    static final int HEIGHT = TILE * 20;
     static final int FRAME = 1000 / 60; // 60 FPS
 }
