5b04daba91f45ffd6c7d87a34dd26454c43d0f99
Shaunak Warty <37036096+hyperXcube@users.noreply.github.com>
2019-10-13 18:41:30 -0700

    (Tetris) Add separate block types

--- src/games/tetris/Block.java
+++ src/games/tetris/Block.java
@@ -14,23 +14,71 @@ class Block {
     Block(Game parent) {
         tetris = parent;
 
-        // Dummy code to initialize block, this needs to be changed later
-        area[0] = new Rectangle(80, 80, 40, 40);
-        area[1] = new Rectangle(120, 80, 40, 40);
-        area[2] = new Rectangle(160, 80, 40, 40);
-        area[3] = new Rectangle(120, 120, 40, 40);
+        area[0] = new Rectangle(Constants.TILE, Constants.TILE);
+        area[1] = new Rectangle(Constants.TILE, Constants.TILE);
+        area[2] = new Rectangle(Constants.TILE, Constants.TILE);
+        area[3] = new Rectangle(Constants.TILE, Constants.TILE);
 
         int blockType = (int) (Math.random() * 7);
-        color = switch (blockType) {
-            case 0 -> new Color(135, 231, 235); // I Block
-            case 1 -> new Color(0, 0, 55); // J Block
-            case 2 -> new Color(255, 163, 51); // L Block
-            case 3 -> new Color(253, 255, 0); // O Block
-            case 4 -> new Color(64, 255, 0); // S Block
-            case 5 -> new Color(128, 0, 128); // T Block
-            case 6 -> new Color(220, 20, 60); // Z Block
+        switch (blockType) {
+            case 0 -> {
+                // I block
+                color = new Color(135, 231, 235);
+                area[0].setLocation(80, 0);
+                area[1].setLocation(120, 0);
+                area[2].setLocation(160, 0);
+                area[3].setLocation(200, 0);
+            }
+            case 1 -> {
+                // J block
+                color = new Color(0, 0, 55);
+                area[0].setLocation(80, 0);
+                area[1].setLocation(80, Constants.TILE);
+                area[2].setLocation(120, Constants.TILE);
+                area[3].setLocation(160, Constants.TILE);
+            }
+            case 2 -> {
+                // L block
+                color = new Color(255, 163, 51);
+                area[0].setLocation(80, Constants.TILE);
+                area[1].setLocation(120, Constants.TILE);
+                area[2].setLocation(160, Constants.TILE);
+                area[3].setLocation(160, 0);
+            }
+            case 3 -> {
+                // O block
+                color = new Color(253, 255, 0);
+                area[0].setLocation(80, 0);
+                area[1].setLocation(120, 0);
+                area[2].setLocation(80, Constants.TILE);
+                area[3].setLocation(120, Constants.TILE);
+            }
+            case 4 -> {
+                // S block
+                color = new Color(64, 255, 0);
+                area[0].setLocation(80, Constants.TILE);
+                area[1].setLocation(120, Constants.TILE);
+                area[2].setLocation(120, 0);
+                area[3].setLocation(160, 0);
+            }
+            case 5 -> {
+                // T block
+                color = new Color(128, 0, 128);
+                area[0] = new Rectangle(80, 80, Constants.TILE, Constants.TILE);
+                area[1] = new Rectangle(120, 80, Constants.TILE, Constants.TILE);
+                area[2] = new Rectangle(160, 80, Constants.TILE, Constants.TILE);
+                area[3] = new Rectangle(120, 120, Constants.TILE, Constants.TILE);
+            }
+            case 6 -> {
+                // Z block
+                color = new Color(220, 20, 60);
+                area[0].setLocation(80, 0);
+                area[1].setLocation(120, 0);
+                area[2].setLocation(120, Constants.TILE);
+                area[3].setLocation(160, Constants.TILE);
+            }
             default -> throw new IllegalArgumentException("Unexpected random value: " + blockType);
-        };
+        }
     }
 
     // Moves block one tile down
@@ -38,8 +86,7 @@ class Block {
         // Checks to see if block can be moved one tile down
         for (int i = 0; i < 4; i++) {
             Rectangle rect = area[i];
-            Rectangle newRect = new Rectangle(rect.x, rect.y, rect.width, rect.height);
-            newRect.y += Constants.TILE;
+            Rectangle newRect = new Rectangle(rect.x, rect.y + Constants.TILE, rect.width, rect.height);
             if (!new Rectangle(Constants.WIDTH, Constants.HEIGHT).contains(newRect)) {
                 deactivate();
                 return;
